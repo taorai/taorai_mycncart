e471fc272eeb8ba583648740eaf4f394a0ef5ca7
Unknown <taorai.asia@gmail.com>
2017-03-25 10:33:25 +0700

    Init

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/taorai/admin/');
+define('HTTP_CATALOG', 'http://localhost/taorai/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/taorai/admin/');
+define('HTTPS_CATALOG', 'http://localhost/taorai/');
+
+// DIR
+define('DIR_APPLICATION', '/home/steven/Downloads/mycncart-mycncart-1.6.0.2/admin/');
+define('DIR_SYSTEM', '/home/steven/Downloads/mycncart-mycncart-1.6.0.2/system/');
+define('DIR_IMAGE', '/home/steven/Downloads/mycncart-mycncart-1.6.0.2/image/');
+define('DIR_LANGUAGE', '/home/steven/Downloads/mycncart-mycncart-1.6.0.2/admin/language/');
+define('DIR_TEMPLATE', '/home/steven/Downloads/mycncart-mycncart-1.6.0.2/admin/view/template/');
+define('DIR_CONFIG', '/home/steven/Downloads/mycncart-mycncart-1.6.0.2/system/config/');
+define('DIR_CACHE', '/home/steven/Downloads/mycncart-mycncart-1.6.0.2/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/steven/Downloads/mycncart-mycncart-1.6.0.2/system/storage/download/');
+define('DIR_LOGS', '/home/steven/Downloads/mycncart-mycncart-1.6.0.2/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/steven/Downloads/mycncart-mycncart-1.6.0.2/system/storage/modification/');
+define('DIR_UPLOAD', '/home/steven/Downloads/mycncart-mycncart-1.6.0.2/system/storage/upload/');
+define('DIR_CATALOG', '/home/steven/Downloads/mycncart-mycncart-1.6.0.2/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'taorai_admin');
+define('DB_PASSWORD', 'bXCr8meDIU8KSxUb');
+define('DB_DATABASE', 'db_taorai_dev');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 't_');
